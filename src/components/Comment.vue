02047ddc28a095a2f319affb6b9bf77d953bfb5b
joke <1095313335@qq.com>
2017-06-12 21:00:57 +0800

    我的资料评论页面

---
+++ src/components/Comment.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="comment">
+	<h1>评论页面</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Comment',
+  methods:{
+
+  }
+}
+</script>
+
+<style>
+</style>
+
+
