612c92d825bf9a29dc10f0b573e6f7fdaf3f6af7
joke <1095313335@qq.com>
2017-06-12 20:59:04 +0800

    我的资料直播页面

---
+++ src/components/Playing.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="playing">
+	<h1>直播页面</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Playing',
+  methods:{
+
+  }
+}
+</script>
+
+<style>
+</style>
+
+
