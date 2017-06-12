2077212c3fa6389ff5767ee50d388ed5f0c25b63
joke <1095313335@qq.com>
2017-06-12 21:00:23 +0800

    我的资料收藏页面

---
+++ src/components/Collect.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="collect">
+	<h1>收藏页面</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Collect',
+  methods:{
+
+  }
+}
+</script>
+
+<style>
+</style>
+
+
