eecd8aa7df2135e740e9714bc75e4c499f2b9a28
“liheBeauty <1176844790@qq.com>
2017-06-12 19:23:34 +0800

    修改主页嵌套路由

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,50 +1,84 @@
 <template>
   <div class="index">
-		<mt-navbar v-model="selected">
-		  <mt-tab-item id="1">段子</mt-tab-item>
-		  <mt-tab-item id="2">图片</mt-tab-item>
-		  <mt-tab-item id="3">视频</mt-tab-item>
-		</mt-navbar>
-		
-		<!-- tab-container -->
-		<mt-tab-container v-model="selected">
-		  <mt-tab-container-item id="1">
-		    <mt-cell v-for="n in 5" :title="'内容 ' + n" />
-		  </mt-tab-container-item>
-		  <mt-tab-container-item id="2">
-		    <mt-cell v-for="n in 4" :title="'测试 ' + n" />
-		  </mt-tab-container-item>
-		  <mt-tab-container-item id="3">
-		    <mt-cell v-for="n in 6" :title="'选项 ' + n" />
-		  </mt-tab-container-item>
-		</mt-tab-container>
+		<!-- 头部选项卡 -->
+  		<hx-tabbar></hx-tabbar>
+		<router-view></router-view>
+		<!-- 段子页面 -->
+		<div id="txt">
+			<header class="txt-header">
+				<img src="" alt="">
+			</header>
+		</div>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
 export default {
   name: 'Index',
 	data(){
 		return {
 			selected:"1"
 		}
+	},
+	components:{
+		"hx-tabbar":{
+			template:`<ul class="am-nav am-nav-pills am-nav-justify">
+			<li>
+				<router-link to="/Text"><span class="hxactive" @click="toggleClass">段子</span></router-link>
+			</li>
+			<li>
+				<router-link to="/Pic"><span @click="toggleClass">图片</span></router-link>
+			</li>
+			<li>
+				<router-link to="/Text"><span @click="toggleClass">视频</span></router-link>
+			</li>
+		</ul>`
+		}
+		
+	},
+	created(){
+		Vue.axios.get("../static/json/duanText.json").then((res)=>{
+			console.log(res.data.data.data);
+		})
+	},
+	methods:{
+		toggleClass:function(event){
+			if(event.target.className=="hxactive"){
+			}else{
+				var arr=document.querySelectorAll(".am-nav li span");
+				for(var i=0;i<3;i++){
+					arr[i].className="";
+				}
+				event.target.className="hxactive";
+			}
+		}
+	},
+	computed:{
+		
 	}
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-	.mint-navbar{
-		background: #dcd9cf;
-		color: #6b5547;
-	}
-	.mint-navbar .mint-tab-item .mint-tab-item-label{
-		font-size: 1.5rem;
-	}
-	.mint-navbar .is-selected .mint-tab-item-label{
-		color: #6b5547;
-		font-weight: bold;
-	}
-
-
+.am-nav.am-nav-pills.am-nav-justify{
+	background:#dcd9cf;
+}
+.am-nav.am-nav-pills.am-nav-justify li{
+	text-align: center;
+	height: 40px;
+	line-height: 40px;
+}
+.am-nav.am-nav-pills.am-nav-justify>li>a{
+	color: #874c0d;
+}
+.am-nav.am-nav-pills.am-nav-justify>li>a:hover{
+	background: none;
+}
+.hxactive{
+	border-bottom:2px solid #493d32;
+	margin-bottom: 1px;
+	display: inline-block; 
+}
 </style>
