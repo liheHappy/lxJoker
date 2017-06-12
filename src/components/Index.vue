e58d8c1ac94a2ff38baf461c9a0f4ebb39b51516
joke <1095313335@qq.com>
2017-06-12 16:01:18 +0800

    黄鑫 主页选项卡的修改

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,16 +1,50 @@
 <template>
   <div class="index">
-    <h1>还能影响你的情绪，是我目前最高兴的事，至少证明我对你不是可有可无。</h1>
-    
+		<mt-navbar v-model="selected">
+		  <mt-tab-item id="1">段子</mt-tab-item>
+		  <mt-tab-item id="2">图片</mt-tab-item>
+		  <mt-tab-item id="3">视频</mt-tab-item>
+		</mt-navbar>
+		
+		<!-- tab-container -->
+		<mt-tab-container v-model="selected">
+		  <mt-tab-container-item id="1">
+		    <mt-cell v-for="n in 5" :title="'内容 ' + n" />
+		  </mt-tab-container-item>
+		  <mt-tab-container-item id="2">
+		    <mt-cell v-for="n in 4" :title="'测试 ' + n" />
+		  </mt-tab-container-item>
+		  <mt-tab-container-item id="3">
+		    <mt-cell v-for="n in 6" :title="'选项 ' + n" />
+		  </mt-tab-container-item>
+		</mt-tab-container>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Index'
+  name: 'Index',
+	data(){
+		return {
+			selected:"1"
+		}
+	}
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style>
+	.mint-navbar{
+		background: #dcd9cf;
+		color: #6b5547;
+	}
+	.mint-navbar .mint-tab-item .mint-tab-item-label{
+		font-size: 1.5rem;
+	}
+	.mint-navbar .is-selected .mint-tab-item-label{
+		color: #6b5547;
+		font-weight: bold;
+	}
+
+
 </style>
