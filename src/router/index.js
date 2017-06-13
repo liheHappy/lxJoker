31ad6d4e475a70952e489ad91598762f61ed7198
joke <1095313335@qq.com>
2017-06-13 15:09:21 +0800

    路由修改

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,7 @@ import Playing from '@/components/Playing'
 import Collect from '@/components/Collect'
 import Comment from '@/components/Comment'
 import Textshow from '@/components/Textshow'
+import Field from '@/components/Field'
 
 Vue.use(Router)
 
@@ -92,6 +93,11 @@ export default new Router({
       path: '/Textshow/:id',
       name: 'Textshow',
       component: Textshow
+    },
+    {
+      path: '/Field',
+      name: 'Field',
+      component: Field
     }
   ]
 })
