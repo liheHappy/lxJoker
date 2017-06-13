9a4c5b7dd29bd340dfcf001c709b47e31b6611f1
joke <1095313335@qq.com>
2017-06-13 17:38:29 +0800

    选择登录方式

--- src/components/Field.vue
+++ src/components/Field.vue
@@ -35,26 +35,26 @@ export default {
 		"hx-field-nav":{
 			template:`
 				<div>
-				<ul class="am-list am-list-static am-list-border">
-		    	<a>
-		    		<li><span><i class="am-icon-mobile am-icon-md"></i></span>手机号</li>
-		    	</a>
-		    	<a>
-		    		<li><span><i class="am-icon-wechat am-icon-sm"></i></span>微信</li>
-		    	</a>
-		    	<a>
-		    		<li><span><i class="am-icon-weibo am-icon-sm"></i></span>新浪微博</li>
-		    	</a>
-		    	<a>
-		    		<li><span><i class="am-icon-qq am-icon-sm"></i></span>腾讯QQ</li>
-		    	</a>
-		    	<a>
-		    		<li><span><i class="am-icon-tencent-weibo am-icon-sm"></i></span>腾讯微博</li>
-		    	</a>
-				</ul>
-				<router-link class="aggrement" to="/Aggrement">
-					<input type="checkbox" checked="checked" />我同意《内涵段子用户使用协议》
-				</router-link>
+					<ul class="am-list am-list-static am-list-border">
+						<router-link to="/Login">
+				    		<li><span><i class="am-icon-mobile am-icon-md"></i></span>手机号</li>
+			    	</router-link>
+			    	<a>
+			    		<li><span><i class="am-icon-wechat am-icon-sm"></i></span>微信</li>
+			    	</a>
+			    	<a>
+			    		<li><span><i class="am-icon-weibo am-icon-sm"></i></span>新浪微博</li>
+			    	</a>
+			    	<a>
+			    		<li><span><i class="am-icon-qq am-icon-sm"></i></span>腾讯QQ</li>
+			    	</a>
+			    	<a>
+			    		<li><span><i class="am-icon-tencent-weibo am-icon-sm"></i></span>腾讯微博</li>
+			    	</a>
+					</ul>
+					<router-link class="aggrement" to="/Aggrement">
+						<input type="checkbox" checked="checked" />我同意《内涵段子用户使用协议》
+					</router-link>
 				</div>
 			`
 		}
@@ -64,7 +64,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-.field{
+.field.am-animation-slide-right{
 	height: 100vh;
 	width: 100%;
 	background: #f5f5f5;
@@ -110,10 +110,10 @@ export default {
 .field .am-list a:nth-of-type(5)>li>span{
 	background: #1ac5de;
 }
-.field>.am-list>a>li{
+.field div .am-list a li{
 	list-style: none;
 }
-.field>.am-list>a>li>span{
+.field div .am-list>a>li>span{
 	display: inline-block;
 	background: #3dcb72;
 	border-radius: 3rem;
