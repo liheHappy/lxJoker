f4c01e0b0f39e4e6beb0282529e4a492cc2d9b8c
“liheBeauty <1176844790@qq.com>
2017-06-13 16:39:27 +0800

    选项卡组件封装

--- src/components/Find.vue
+++ src/components/Find.vue
@@ -1,20 +1,7 @@
 <template>
   <div class="find">
-	  	<!-- <mt-header title="发现">
-		  <router-link to="/" slot="left">
-		    <mt-button><span class="iconfont icon-sousuo-sousuo"></span></mt-button>
-		  </router-link>
-		  <mt-button slot="right"><span class="iconfont icon-dingwei"></span></mt-button>
-		</mt-header> -->
 		<!-- 头部 -->
-		<header class="findTop">
-			 <span class="iconfont icon-sousuo-sousuo"></span>
-			 <p>
-			 	<button class="findButton left">热吧</button>
-			 	<button class="findButton right">订阅</button>
-			 </p>
-			 <span class="iconfont icon-dingwei"></span>
-		</header>
+  		<hx-header-tab></hx-header-tab>
 		<!-- 轮播图 -->
 		<mt-swipe :auto="4000">
 		  <mt-swipe-item>
@@ -28,7 +15,8 @@
 		  </mt-swipe-item>
 		</mt-swipe>
 		<!--发现界面-->
-		<hx-find-list></hx-find-list>
+		
+		<!-- <img src="../assets/head1.jpg" /> -->
   </div>
 </template>
 
@@ -38,42 +26,60 @@ import jsonp from 'jsonp'
 export default {
   name: 'Find',
   components:{
-  	"hx-find-list":{
-  		template:`
-		    <div>
-					<div class="head" v-for="items in arr">
-							<img :src="items.img" />
-					</div>
-		    </div>
-  		`,
-	  	data(){
-				return {
-					arr:""
+  	"hx-header-tab":{
+  		template:`<div class="am-tabs " data-am-tabs>
+				  <ul class="am-tabs-nav am-nav am-nav-tabs">
+				  	<span class="iconfont icon-sousuo-sousuo "></span>
+				  		<li></li>
+				  		<li></li>
+				  		<li class="am-active"><a href="#tab1">热吧</a></li>
+				    	<li><a href="#tab2">订阅</a></li>
+				    	<li></li>
+				    	<li></li>
+				   <span class="iconfont icon-dingwei"></span>
+				  </ul>
+				  <div class="am-tabs-bd">
+				    <div class="am-tab-panel am-active" id="tab1">
+				    	<hx-find-list></hx-find-list>
+				    </div>
+				    <div class="am-tab-panel" id="tab2">222</div>
+				  </div>
+				</div>`,
+		components:{
+			"hx-find-list":{
+		  		template:`<div>
+							<div class="head" v-for="items in arr">
+									<img :src="items.images" />
+									{{items.images}}
+							</div>
+				    	</div>`,
+			  	data(){
+					return {
+						arr:""
+					}
+				},
+				created(){
+					Vue.axios.get("../static/json/find.json").then((res)=>{
+						//console.log(res.data.head)
+						return res.data.head;
+					}).then((data)=>{
+						this.arr = data;
+						console.log(data[0].images)
+						//console.log(this.arr);
+						//console.log(data.head);
+					})
 				}
-			},
-			created(){
-				Vue.axios.get("../static/json/find.json").then((res)=>{
-					//console.log(res.data.head)
-					return res.data.head;
-				}).then((data)=>{
-					this.arr = data;
-					//console.log(this.arr);
-					//console.log(data.head);
-				})
-			}
+		  	}
+		}
   	}
-  }
+  	
+  }	//components结尾
 }
 </script>
 
 <style>
 .findTop{
 	height: 40px;
-	display: flex;
-	justify-content: space-around;
-	align-items: center;
-	background: #dcd9cf;
-	color: #493d32;
 }
 .findButton{
 	color: #493d32;
@@ -101,5 +107,25 @@ export default {
 .mint-swipe-item img{
 	height: 100%;
 	width: 100%;
+}
+
+..am-tabs-bd{
+	border: none;
+}
+.am-tabs-nav.am-nav.am-nav-tabs{
+	display: flex;
+	justify-content: space-around;
+	align-items: center;
+	background: #dcd9cf;
+	color: #493d32;
+}
+.am-nav>li>a{
+	padding: .2rem .5rem;
+	margin: .5rem 0;
+	color: #493d32;
+}
+.am-nav-tabs>li.am-active>a, .am-nav-tabs>li.am-active>a:focus, .am-nav-tabs>li.am-active>a:hover{
+	background: pink;
+	border-radius: 7px;
 }
 </style>
