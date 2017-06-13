4f251b94dacafb307eb45709ccd36ce256d5e2e0
joke <1095313335@qq.com>
2017-06-13 15:11:46 +0800

    Field修改

---
+++ src/components/Field.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="field">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Field',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.field{
+	height: 100vh;
+	width: 100%;
+	background: #fff;
+	position: absolute;
+	z-index: 1000;
+}
+</style>
