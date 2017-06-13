ac6dd156da02f029fa003ad874c5104c34e2ba72
“liheBeauty <1176844790@qq.com>
2017-06-13 18:30:24 +0800

    信息页面组件的大体完成

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,23 +1,7 @@
 <template>
   <div class="message">
-  	<mt-header title="消息">
-	  <mt-button slot="right">
-	  	<router-link to="/Aggrement">黑名单</router-link>
-	  </mt-button>
-	</mt-header>
-	<mt-tab-container v-model="active">
-	  <mt-tab-container-item id="tab-container1">
-	    <mt-cell title="投稿互动">
-	    	<span class="iconfont icon-iconfontqianjin"></span>
-	    </mt-cell>
-	    <mt-cell title="系统消息">
-	    	<span class="iconfont icon-iconfontqianjin"></span>
-	    </mt-cell>
-	    <mt-cell title="粉丝关注">
-	    	<span class="iconfont icon-iconfontqianjin"></span>
-	    </mt-cell>
-	  </mt-tab-container-item>
-	</mt-tab-container>
+	<hx-msg-header></hx-msg-header>
+	<hx-message-list></hx-message-list>
   </div>
 </template>
 
@@ -28,6 +12,42 @@ export default {
   	return {
   		active:"tab-container1"
   	}
+  },
+  components:{
+  	"hx-msg-header":{
+  		template:`<div>
+  					<header data-am-widget="header"
+			          class="am-header am-header-default">
+			      <h1 class="am-header-title">
+			          消息
+			      </h1>
+			      <div class="am-header-right am-header-nav">
+			      	<router-link to="/Field">黑名单</router-link>
+			      </div>
+			  </header>
+			  </div>`,
+		methods:{
+			goBack(){
+				window.history.back();
+			}
+		}
+  	},
+  	"hx-message-list":{
+  		template:`<div>
+		<ul class="am-list am-list-static am-list-border">
+	    	<router-link to="/Field">
+	    		<li><span><i class="am-icon-pencil am-icon-sm"></i></span>投稿互动<i class="am-icon-angle-right"></i></li>
+	    	</router-link>
+	    	<router-link to="/Field">
+	    		<li><span><i class="am-icon-bullhorn am-icon-sm"></i></span>系统消息<i class="am-icon-angle-right"></i></li>
+	    	</router-link>
+	    	<router-link to="/Field">
+	    		<li><span><i class="am-icon-heartbeat am-icon-sm"></i></span>粉丝关注<i class="am-icon-angle-right"></i></li>
+	    	</router-link>
+		</ul>
+	</div>`
+  	}
+
   }
 }
 </script>
@@ -37,5 +57,63 @@ export default {
 .mint-button-text a{
 	text-decoration: none;
 	color: #493d32;
+}
+.am-header.am-header-default{
+	background:#dcd9cf;
+	position: fixed;
+	z-index: 100;
+}
+.am-header-default .am-header-title{
+	color: #874c0d;
+}
+.am-header-default .am-header-title{
+	font-size: 1.7rem;
+}
+.am-header-default .am-header-nav>a{
+	color: #874c0d;
+}
+
+.message .am-list{
+	padding-top: 49px;
+}
+.message .am-list a{
+	display: block;
+	padding: 1rem;
+	background: #ffffff;
+	color: black;
+	text-align: left;
+	margin-bottom: 0.1rem;
+}
+.message .am-list a:nth-of-type(2)>li>span{
+	background: #6ec850;
+}
+.message .am-list a:nth-of-type(3)>li>span{
+	background: #ff5454;
+}
+.message .am-list a:nth-of-type(4)>li>span{
+	background: #40a7ff;
+}
+.message .am-list a:nth-of-type(5)>li>span{
+	background: #1ac5de;
+}
+.message div .am-list a li{
+	list-style: none;
+}
+.message div .am-list>a>li>span{
+	display: inline-block;
+	background: #3dcb72;
+	border-radius: 3rem;
+	width: 3rem;
+	height: 3rem;
+	text-align: center;
+	line-height: 3rem;
+	padding: 0;
+	color: white;
+	margin-right: 1rem;
+}
+
+.am-icon-angle-right{
+	float: right;
+	font-size: 2rem;
 }
 </style>
