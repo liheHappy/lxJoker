89f8e2c1c62b97da8c07358b86d6d326e0aee2c3
joke <1095313335@qq.com>
2017-06-13 12:32:19 +0800

    发现界面修改

--- src/components/Find.vue
+++ src/components/Find.vue
@@ -1,39 +1,68 @@
 <template>
   <div class="find">
-  	<!-- <mt-header title="发现">
-	  <router-link to="/" slot="left">
-	    <mt-button><span class="iconfont icon-sousuo-sousuo"></span></mt-button>
-	  </router-link>
-	  <mt-button slot="right"><span class="iconfont icon-dingwei"></span></mt-button>
-	</mt-header> -->
-	<!-- 头部 -->
-	<header class="findTop">
-		 <span class="iconfont icon-sousuo-sousuo"></span>
-		 <p>
-		 	<button class="findButton left">热吧</button>
-		 	<button class="findButton right">订阅</button>
-		 </p>
-		 <span class="iconfont icon-dingwei"></span>
-	</header>
-	<!-- 轮播图 -->
-	<mt-swipe :auto="4000">
-	  <mt-swipe-item>
-		<img src="../assets/find1.jpg">
-	  </mt-swipe-item>
-	  <mt-swipe-item>
-	  	<img src="../assets/find2.jpg">
-	  </mt-swipe-item>
-	  <mt-swipe-item>
-		<img src="../assets/find3.jpg">
-	  </mt-swipe-item>
-	</mt-swipe>
-    
+	  	<!-- <mt-header title="发现">
+		  <router-link to="/" slot="left">
+		    <mt-button><span class="iconfont icon-sousuo-sousuo"></span></mt-button>
+		  </router-link>
+		  <mt-button slot="right"><span class="iconfont icon-dingwei"></span></mt-button>
+		</mt-header> -->
+		<!-- 头部 -->
+		<header class="findTop">
+			 <span class="iconfont icon-sousuo-sousuo"></span>
+			 <p>
+			 	<button class="findButton left">热吧</button>
+			 	<button class="findButton right">订阅</button>
+			 </p>
+			 <span class="iconfont icon-dingwei"></span>
+		</header>
+		<!-- 轮播图 -->
+		<mt-swipe :auto="4000">
+		  <mt-swipe-item>
+			<img src="../assets/find1.jpg">
+		  </mt-swipe-item>
+		  <mt-swipe-item>
+		  	<img src="../assets/find2.jpg">
+		  </mt-swipe-item>
+		  <mt-swipe-item>
+			<img src="../assets/find3.jpg">
+		  </mt-swipe-item>
+		</mt-swipe>
+		<!--发现界面-->
+		<hx-find-list></hx-find-list>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+import jsonp from 'jsonp'
 export default {
-  name: 'Find'
+  name: 'Find',
+  components:{
+  	"hx-find-list":{
+  		template:`
+		    <div>
+					<div class="head" v-for="items in arr">
+							<img :src="items.img" />
+					</div>
+		    </div>
+  		`,
+	  	data(){
+				return {
+					arr:""
+				}
+			},
+			created(){
+				Vue.axios.get("../static/json/find.json").then((res)=>{
+					//console.log(res.data.head)
+					return res.data.head;
+				}).then((data)=>{
+					this.arr = data;
+					//console.log(this.arr);
+					//console.log(data.head);
+				})
+			}
+  	}
+  }
 }
 </script>
 
