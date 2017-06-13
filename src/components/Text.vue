f1ed289f31993ce1aecdd237f8ffaffd994c02e6
“liheBeauty <1176844790@qq.com>
2017-06-13 11:13:06 +0800

    实现路由跳转到详情页并传参功能C

--- src/components/Text.vue
+++ src/components/Text.vue
@@ -13,6 +13,7 @@ export default {
 		"hx-text-list":{
 			template:`<div>
 					<div class="txt" v-for="items in arr">
+					<router-link :to="{name:'Textshow',params:{id:items.group.group_id}}">
 						<header class="txt-header">
 							<img :src="items.group.user.avatar_url" class="am-comment-avatar" width="48" height="48">
 							<span>{{items.group.user.name}}</span>
@@ -29,6 +30,7 @@ export default {
 							<i class="am-icon-share">
 							{{items.group.share_count}}</i>
 						</div>
+					</router-link>
 					</div>
 				</div>`,
 		  	data(){
@@ -41,7 +43,6 @@ export default {
 			      return res.data.data.data;
 			    }).then((data)=>{
 			      this.arr=data;
-			      console.log(this.arr)
 			    })
 			}
 		}
@@ -53,51 +54,11 @@ export default {
 .text{
 	padding-top: 56px;
 }
-.txt{
+.txt a{
 	background: #fff;
 	overflow: auto;
 	margin-bottom: .5rem;
-}
-.txt-header{
-	padding-top: .5rem;
-	height: 40px;
-	line-height: 40px;
-	margin-bottom: .5rem;
-}
-.txt-header img{
-	margin-top: 4px;
-	margin-left: 1rem;
-	margin-right: 0.5rem;
-}
-.hasBorder{
-	border: 1px solid #000;
-	height: 16px;
-	width: 16px;
-	float: right;
-	text-align: center;
-	line-height: 12px;
-	margin-top: 11px;
-	margin-right: 1rem;
-}
-.am-article-lead{
-	margin: 0;
-	background: none;
-}
-.neihan{
-	border: 1px solid #ccc;
-	background: #eee;
-	border-radius: 5px;
-	color: #996c33;
-	padding:.1rem .3rem;
-	margin: .2rem .3rem;
-	font-size: .8rem;
-}
-.am-icon-share{
-	float: right;
-}
-.txt div i{
-	padding: .8rem;
-	color: #ccc;
+	color: #000;
 }
 </style>
 
