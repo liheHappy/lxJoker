c00f0dff4a3526ba86f98eb9849f2d37641d770f
joke <1095313335@qq.com>
2017-06-13 19:17:05 +0800

    手机登录

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,60 +1,50 @@
 <template>
 	
-  <div class="field am-animation-slide-right">
-  	<hx-field-header></hx-field-header>
-  	<hx-field-nav></hx-field-nav>
+  <div class="login am-animation-slide-right">
+  	<hx-Login-header></hx-Login-header>
+  	<hx-Login-input></hx-Login-input>
 
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Field',
+  name: 'Login',
   data () {
     return {
     	
     }
   },
 	components:{
-		"hx-field-header":{
+		"hx-Login-header":{
 			template:`
-				<div>
-					<header data-am-widget="header" class="am-header am-header-default">
-			      <div class="am-header-left am-header-nav" @click="goBack">
-			  			<i class=" fa am-icon-angle-left"></i>
-			      </div>
-			      <h1 class="am-header-title">登陆</h1>
-				  </header>
-			 	</div>`,
+			 	<div>
+  				  <header data-am-widget="header" class="am-header am-header-default">
+			        <div class="am-header-left am-header-nav" @click="goBack">
+			  		  <i class=" fa am-icon-angle-left"></i>
+			        </div>
+			        <h1 class="am-header-title">手机号登陆</h1>
+				    <div class="am-header-right am-header-nav">
+				      <router-link class="register" to="">注册</router-link>
+				    </div>
+			  	  </header>
+			    </div>
+			 	`,
 			methods:{
 				goBack(){
 					window.history.back();
 				}
 			}
 		},
-		"hx-field-nav":{
+		"hx-Login-input":{
 			template:`
 				<div>
-					<ul class="am-list am-list-static am-list-border">
-						<router-link to="/Login">
-				    		<li><span><i class="am-icon-mobile am-icon-md"></i></span>手机号</li>
-			    	</router-link>
-			    	<a>
-			    		<li><span><i class="am-icon-wechat am-icon-sm"></i></span>微信</li>
-			    	</a>
-			    	<a>
-			    		<li><span><i class="am-icon-weibo am-icon-sm"></i></span>新浪微博</li>
-			    	</a>
-			    	<a>
-			    		<li><span><i class="am-icon-qq am-icon-sm"></i></span>腾讯QQ</li>
-			    	</a>
-			    	<a>
-			    		<li><span><i class="am-icon-tencent-weibo am-icon-sm"></i></span>腾讯微博</li>
-			    	</a>
-					</ul>
-					<router-link class="aggrement" to="/Aggrement">
-						<input type="checkbox" checked="checked" />我同意《内涵段子用户使用协议》
-					</router-link>
+				  <div class="logininput">
+				    <mt-field label="手机号" placeholder="请输入你的手机号" type="tel"></mt-field>
+				    <mt-field label="密码" placeholder="输入密码" type="password"></mt-field>
+				    <mt-button size="large" type="danger">large</mt-button>
+				    <router-link class="forget" to="">忘记密码</router-link>
+				  </div>
 				</div>
 			`
 		}
@@ -64,7 +54,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-.field.am-animation-slide-right{
+.login.am-animation-slide-right{
 	height: 100vh;
 	width: 100%;
 	background: #f5f5f5;
@@ -72,8 +62,29 @@ export default {
 	z-index: 1000;
 	text-align: center;
 }
-.aggrement{
-	color: black;
+.login .logininput{
+	margin-top: 49px;
+	padding-top: 1rem;
+}
+.login .logininput .forget{
+	float: right;
+}
+.login .logininput .mint-button{
+	margin-top: 2rem;
+	width: 90vw;
+	margin-left: 5vw;
+}
+.mint-cell .mint-cell-wrapper{
+	padding: 0;
+}
+.mint-cell .mint-cell-wrapper .mint-cell-title{
+	padding-left: 1rem;
+}
+.login.am-animation-slide-right{
+	text-align: left;
+}
+.am-header-default .am-header-nav .register{
+	color: #874c0d;
 }
 .am-header.am-header-default{
 	background:#dcd9cf;
@@ -83,46 +94,7 @@ export default {
 .am-header-left.am-header-nav,.am-header-default .am-header-title,.am-header-right.am-header-nav{
 	color: #874c0d;
 }
-.field .am-list{
-	margin-top: 49px;
-	padding-top: 1rem;
-}
-.field .am-list a:nth-of-type(1){
-	margin-bottom: 1rem;
-}
-.field .am-list a{
-	display: block;
-	padding: 1rem;
-	background: #ffffff;
+.aggrement{
 	color: black;
-	text-align: left;
-	margin-bottom: 0.1rem;
-}
-.field .am-list a:nth-of-type(2)>li>span{
-	background: #6ec850;
-}
-.field .am-list a:nth-of-type(3)>li>span{
-	background: #ff5454;
-}
-.field .am-list a:nth-of-type(4)>li>span{
-	background: #40a7ff;
-}
-.field .am-list a:nth-of-type(5)>li>span{
-	background: #1ac5de;
-}
-.field div .am-list a li{
-	list-style: none;
-}
-.field div .am-list>a>li>span{
-	display: inline-block;
-	background: #3dcb72;
-	border-radius: 3rem;
-	width: 3rem;
-	height: 3rem;
-	text-align: center;
-	line-height: 3rem;
-	padding: 0;
-	color: white;
-	margin-right: 1rem;
 }
 </style>
