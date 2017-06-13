ce8c6031b2fdc35783cccdf09f650fb4e3186488
“liheBeauty <1176844790@qq.com>
2017-06-13 11:15:30 +0800

    修改页面样式

--- src/components/Text.vue
+++ src/components/Text.vue
@@ -60,6 +60,52 @@ export default {
 	margin-bottom: .5rem;
 	color: #000;
 }
+.imgBorder{
+	min-height: 30vh;
+	width: 100%;
+	margin-top: .5rem;
+}
+.txt-header{
+	padding-top: .5rem;
+	height: 40px;
+	line-height: 40px;
+	margin-bottom: .5rem;
+}
+.txt-header img{
+	margin-top: 4px;
+	margin-left: 1rem;
+	margin-right: 0.5rem;
+}
+.hasBorder{
+	border: 1px solid #000;
+	height: 16px;
+	width: 16px;
+	float: right;
+	text-align: center;
+	line-height: 12px;
+	margin-top: 11px;
+	margin-right: 1rem;
+}
+.am-article-lead{
+	margin: 0;
+	background: none;
+}
+.neihan{
+	border: 1px solid #ccc;
+	background: #eee;
+	border-radius: 5px;
+	color: #996c33;
+	padding:.1rem .3rem;
+	margin: .2rem .3rem;
+	font-size: .8rem;
+}
+.am-icon-share{
+	float: right;
+}
+.txt div i{
+	padding: .8rem;
+	color: #ccc;
+}
 </style>
 
 
