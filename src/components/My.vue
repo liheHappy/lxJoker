34afaf7e3146377e69a36f06d775579ec19023e9
joke <1095313335@qq.com>
2017-06-14 10:56:35 +0800

    我的页面修改

--- src/components/My.vue
+++ src/components/My.vue
@@ -9,9 +9,7 @@
 		<!--头像组件-->
 		<hx-my-means></hx-my-means>
 		<!--选项卡组件-->
-	  	<hx-my-tabbar></hx-my-tabbar>
-
-		
+	  <hx-my-tabbar></hx-my-tabbar>
 
   </div>
 </template>
@@ -26,39 +24,39 @@ export default {
 	},
 	components:{
 		"hx-my-tabbar":{
-			template:`<div>
-						<div class="am-tabs" data-am-tabs>
-						  <ul class="am-tabs-nav am-nav am-nav-tabs">
-						  	  <li class="zhanPos"></li>
-							  <li  class="am-active">
-							  	<a href="#tab1"><span class="lhactive" @click="toggleClass">投稿</span></a>
-							  </li>
-							  <li>
-							  	<a href="#tab2"><span @click="toggleClass">直播</span></a>
-							  </li>
-							  <li>
-							  	<a href="#tab3"><span @click="toggleClass">收藏</span></a>
-							  </li>
-							  <li>
-							  	<a href="#tab4"><span @click="toggleClass">评论</span></a>
-							  </li>
-						  </ul>
-						  <div class="am-tabs-bd">
-						    <div class="am-tab-panel am-fade am-in am-active" id="tab1">
-						      置身人群中<br>你只需要被淹没 享受 沉默<br>退到人群后<br>你只需给予双手 微笑 等候
-						    </div>
-						    <div class="am-tab-panel am-fade" id="tab2">
-						      走在忠孝东路<br>徘徊在茫然中<br>在我的人生旅途<br>选择了多少错误<br>我在睡梦中惊醒<br>感叹悔言无尽<br>恨我不能说服自己<br>接受一切教训<br>让生命去等候<br>等候下一个漂流<br>让生命去等候<br>等候下一个伤口
-						    </div>
-						    <div class="am-tab-panel am-fade" id="tab3">
-						      我就这样告别山下的家，我实在不愿轻易让眼泪留下。我以为我并不差不会害怕，我就这样自己照顾自己长大。我不想因为现实把头低下，我以为我并不差能学会虚假。怎样才能够看穿面具里的谎话？别让我的真心散的像沙。如果有一天我变得更复杂，还能不能唱出歌声里的那幅画？
-						    </div>
-						    <div class="am-tab-panel am-fade" id="tab4">
-						       你还要我怎样
-						    </div>
-						  </div>
-						</div>
-					   </div>`,
+			template:`
+				<div>
+					<div class="am-tabs mynav" data-am-tabs>
+					  <ul class="am-tabs-nav am-nav am-nav-tabs">
+						  <li  class="am-active">
+						  	<a href="#tab1"><span class="lhactive" @click="toggleClass">投稿</span></a>
+						  </li>
+						  <li>
+						  	<a href="#tab2"><span @click="toggleClass">直播</span></a>
+						  </li>
+						  <li>
+						  	<a href="#tab3"><span @click="toggleClass">收藏</span></a>
+						  </li>
+						  <li>
+						  	<a href="#tab4"><span @click="toggleClass">评论</span></a>
+						  </li>
+					  </ul>
+						<div class="am-tabs-bd">
+					    <div class="am-tab-panel am-fade am-in am-active" id="tab1">
+					      
+					    </div>
+					    <div class="am-tab-panel am-fade" id="tab2">
+					      
+					    </div>
+					    <div class="am-tab-panel am-fade" id="tab3">
+					      
+					    </div>
+					    <div class="am-tab-panel am-fade" id="tab4">
+					      
+					    </div>
+					  </div>
+					</div>
+				</div>`,
 			methods:{
 				toggleClass:function(event){
 					if(event.target.className=="lhactive"){
@@ -104,56 +102,65 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scope>
-	.my{
-		overflow-x: hidden;
-		width: 100%;
-	}
-	.am-g{
-		background: #FFFFFF;
-		padding-top: 1rem;
-		padding-bottom: 1rem;
-	}
-	.am-icon-btn{
-		width: 9rem;
-    height: 9rem;
-    font-size: 11rem;
-    line-height: 9.1rem;
-		color: #FFFFFF;
-	}
-	.am-u-sm-8 .field mt-button{
-		display: block;
-	}
-	.am-u-sm-8 .am-u-sm-4{
-		margin-bottom: 1rem;
-	}
-	.am-u-sm-8 .am-u-sm-4 span{
-		display: block;
-		text-align: center;
-		color: black;
-	}
-	.lhactive{
-		border-bottom:2px solid hotpink;
-		margin-bottom: 1px;
-		display: inline-block; 
-	}
-	.am-nav-tabs{
-		border: none;
-	}
-	.am-tabs-bd{
-		border: none;
-	}
-	.am-nav-tabs>li.am-active>a, .am-nav-tabs>li.am-active>a:focus, .am-nav-tabs>li.am-active>a:hover{
-		border: none;
-	}
-	.am-tabs-nav.am-nav.am-nav-tabs{
-		background: #fff;
-	}
-	.am-nav-tabs>li>a{
-		color: #555;
-	}
-	.zhanPos{
-		width: 2rem;
-		height: 2rem;
-	}
+<style>
+.my{
+	overflow-x: hidden;
+	height: 100vh;
+	width: 100%;
+	background: url(../../static/img/login1.png) no-repeat 0 22rem;
+	background-size: contain;
+}
+.am-g{
+	background: #FFFFFF;
+	padding-top: 1rem;
+	padding-bottom: 1rem;
+}
+
+.am-icon-btn{
+	width: 8rem;
+	height: 8rem;
+	font-size: 10rem;
+	line-height: 8.1rem;
+	color: #FFFFFF;
+}
+.am-u-sm-8 .am-u-sm-4{
+	margin-bottom: 1rem;
+}
+.am-u-sm-8 .am-u-sm-4 span{
+	display: block;
+	text-align: center;
+	color: black;
+}
+.lhactive{
+	border-bottom:2px solid hotpink;
+	margin-bottom: 1px;
+	display: inline-block; 
+}
+.mynav .am-tabs-bd #tab1{
+	background: #eeeeee;
+}
+.am-tabs-bd{
+	border: none;
+}
+.mynav>.am-nav-tabs>li.am-active>a, .am-nav-tabs>li.am-active>a:focus, .am-nav-tabs>li.am-active>a:hover{
+	background: #FFFFFF !important;
+	border: none;
+}
+
+.am-nav-tabs>li>a{
+	color: #555;
+	padding: .2rem .5rem;
+}
+.zhanPos{
+	width: 2rem;
+	height: 2rem;
+}
+.mynav>.am-tabs-nav.am-nav.am-nav-tabs{
+	background: #FFFFFF;
+	border: none;
+	display: flex;
+	justify-content: space-around;
+	margin-top: 0.3rem;
+}
+
 </style>
