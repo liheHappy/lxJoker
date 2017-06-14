ac20bb18a215bc4c07483f9f95ef1f502e218c7c
“liheBeauty <1176844790@qq.com>
2017-06-14 23:30:58 +0800

    不知道做了什么

--- src/Hot.vue
+++ src/Hot.vue
@@ -120,14 +120,14 @@ body{
   color: #874c0d;
   background: none;
 }
-#loading{
+/* #loading{
   background: url('./../static/img/index.png') no-repeat;
   background-size: 100% 100%;
   position: absolute;
   height: 100vh;
   width: 100%;
   z-index: 1000;
-}
+} */
 
 /* .mint-cell-swipe .mint-cell-wrapper, .mint-cell-swipe .mint-cell-left, .mint-cell-swipe .mint-cell-right {
     -webkit-transition: -webkit-transform 1000ms ease-in-out;
