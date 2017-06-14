01988e16e6d940a7ea85bf4430ff1009d31a2bc2
“liheBeauty <1176844790@qq.com>
2017-06-14 23:44:47 +0800

    注释

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,17 +1,35 @@
 <template>
   <div class="search">
-		<!-- 头部 -->
-  		<p>我真是疯了</p>
+  <!-- 搜索栏 -->
+		<mt-search
+		  v-model="value"
+		  placeholder="搜索">
+		</mt-search>
+		<!-- 背景图片 -->
+		<div class="bg"></div>
   </div>
 </template>
 
 <script>
 import Vue from 'vue'
 export default {
-  name: 'Search'
+  name: 'Search',
+  value:""
 }
 </script>
 
-<style>
-
+<style scoped>
+.search{
+	height: 100vh;
+	width: 100%;
+	background:#eee;
+	position: absolute;
+	z-index: 100;
+}
+.search .bg{
+	background: url('./../../static/img/jie1.png') no-repeat;
+	background-size: 100% 100%;
+	width: 100%;
+	height: 70vh;
+}
 </style>
