036426d5502d67a3755494655e576712f2540add
“liheBeauty <1176844790@qq.com>
2017-06-14 23:30:23 +0800

    增加页面

---
+++ src/components/Search.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="search">
+		<!-- 头部 -->
+  		<p>我真是疯了</p>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+export default {
+  name: 'Search'
+}
+</script>
+
+<style>
+
+</style>
