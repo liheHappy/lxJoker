5d927aa5a230dab7edcb995d57daee731df47438
joke <1095313335@qq.com>
2017-06-15 16:54:07 +0800

    页面修改

--- src/components/My.vue
+++ src/components/My.vue
@@ -3,18 +3,20 @@
 		<mt-header>
 		  <mt-button slot="right">
 		  	<router-link class="Myset" to="/Set">
-		  		<span class="iconfont icon-icon"></span>
+		  		<span class="am-icon am-icon-shopping-bag"></span>
 		  	</router-link>
 		  	<router-link class="Myset" to="/Set">
-		  		<span class="iconfont icon-shezhi"></span>
+		  		<span class="am-icon am-icon-cogs"></span>
 		  	</router-link>
-		  	
+		  	<!--mars   diamond   fire-->
 		  </mt-button>
 		</mt-header>
 		<!--头像组件-->
-		<hx-my-means></hx-my-means>
+		<!--<hx-my-means></hx-my-means>-->
+		<hx-my-data></hx-my-data>
+
 		<!--选项卡组件-->
-	  	<hx-my-tabbar></hx-my-tabbar>
+	  <hx-my-tabbar></hx-my-tabbar>
 
   </div>
 </template>
@@ -78,12 +80,46 @@ export default {
 		"hx-my-means":{
 			template:`
 				<div class="am-g">
-				  	<div class="am-u-sm-4">
-					  	<router-link to="/Userinfo">
-					  		<span class="am-icon-btn am-icon-user"></span>
-					  	</router-link>
-				  	</div>
-				  	<div class="am-u-sm-8">
+			  	<div class="am-u-sm-4">
+				  	<router-link to="/Userinfo">
+				  		<span class="am-icon-btn am-icon-user"></span>
+				  	</router-link>
+			  	</div>
+			  	<div class="am-u-sm-8">
+			  		<a href="" class="am-u-sm-4">
+			  			<span>0</span>
+			  			<span>粉丝</span>
+			  		</a>
+			  		<a href="" class="am-u-sm-4">
+			  			<span>0</span>
+			  			<span>关注</span>
+			  		</a>
+			  		<a href="" class="am-u-sm-4">
+			  			<span>0</span>
+			  			<span>积分</span>
+			  		</a>
+			  		<router-link class="field" to="/Field">
+			  			<mt-button type="danger" size="large" plain>立即登录</mt-button>
+			  		</router-link>
+			  	</div>
+			  </div>
+			`
+		},
+		"hx-my-data":{
+			template:`
+				<div class="am-g my-am-g">
+			  	<div class="am-u-sm-4">
+				  	<router-link to="/Userinfo">
+				  		<img src="../../static/img/Myhead0.jpg" />
+				  	</router-link>
+			  	</div>
+			  	<div class="am-u-sm-8">
+			  		<h4>
+			  			<span>王萌萌</span>
+			  			<i class="am-icon-mars"></i>
+			  		</h4>
+			  		<p class="am-icon-map-marker"> 北京</p>
+			  		<p class="Mydata">
 				  		<a href="" class="am-u-sm-4">
 				  			<span>0</span>
 				  			<span>粉丝</span>
@@ -93,15 +129,21 @@ export default {
 				  			<span>关注</span>
 				  		</a>
 				  		<a href="" class="am-u-sm-4">
-				  			<span>0</span>
+				  			<span>1</span>
 				  			<span>积分</span>
 				  		</a>
-				  		<router-link class="field" to="/Field">
-				  			<mt-button type="danger" size="large" plain>立即登录</mt-button>
-				  		</router-link>
-				  	</div>
+			  		</p>
 			  	</div>
-				`
+			  	<div class="Myfans">
+			  		<span class="am-icon-diamond"> 
+			  			<i> 送出钻石：0</i>
+			  		</span>
+			  		<span class="am-icon-fire"> 
+			  			<i> 获得火力：0</i>
+			  		</span>
+			  	</div>
+				</div>
+			`
 		}
 	}
 }
@@ -110,15 +152,16 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
+/*登录前CSS样式*/
 .Myset{
-	color: #493d32;
+	color: #7e7c78;
 	font-weight: normal;
 }
-.Myset .iconfont{
-	font-size: 26px;
+.Myset .am-icon{
+	font-size: 20px;
 }
-.Myset .icon-shezhi{
-	font-size: 22px;
+.Myset .am-icon-cogs{
+	margin-left: 10px;
 }
 .my{
 	overflow-x: hidden;
@@ -132,7 +175,6 @@ export default {
 	padding-top: 1rem;
 	padding-bottom: 1rem;
 }
-
 .my .am-icon-btn{
 	width: 8rem;
 	height: 8rem;
@@ -146,7 +188,10 @@ export default {
 .my .am-u-sm-8 .am-u-sm-4 span{
 	display: block;
 	text-align: center;
-	color: black;
+	color: #333333;
+}
+.my .am-u-sm-8 .am-u-sm-4 span:nth-of-type(2){
+	color: #666666;
 }
 .my .lhactive{
 	border-bottom:2px solid hotpink;
@@ -180,4 +225,57 @@ export default {
 	margin-top: 0.3rem;
 }
 
+/*登陆后CSS样式*/
+.my img{
+	width: 8rem;
+	height: 8rem;
+	border-radius: 8rem;
+}
+.my .my-am-g{
+	padding-bottom: 0;
+}
+.my .am-u-sm-8 h4{
+	margin: 0;
+}
+.my .am-u-sm-8 h4 span{
+	margin-right: 1rem;
+}
+
+.my .am-u-sm-8 h4 i{
+	height: 1.5rem;
+	width: 1.5rem;
+	text-align: center;
+	line-height: 1.5rem;
+	background: #3bb9e0;
+	display: inline-block;
+	font-size: 14px;
+	color: white;
+	border-radius: 0.4rem;
+}
+.my .am-u-sm-8 .am-icon-map-marker{
+	margin: 0;
+	line-height: 1.4rem;
+	color: #FFFFFF;
+	font-size: 1rem;
+	background: #999999;
+	padding: 0rem 0.6rem 0rem 0.6rem;
+	border-radius: 1rem;
+}
+.my .am-u-sm-8 .Mydata{
+	margin: 0;
+}
+.my .Myfans{
+	padding: 0rem 1rem 0.6rem 1rem;
+}
+.my .Myfans span:nth-of-type(1){
+	margin-right: 4rem;
+	color: #1173ec;
+}
+.my .Myfans span:nth-of-type(2){
+	color: #f56900;
+}
+.my .Myfans span i{
+	font-style: normal;
+	color: #333333;
+}
 </style>
