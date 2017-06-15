35f4afaf2c608f0498afa3e8ec378b8b333d5a88
“liheBeauty <1176844790@qq.com>
2017-06-15 10:01:44 +0800

    搜索页面的完成

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,10 +1,18 @@
 <template>
   <div class="search">
   <!-- 搜索栏 -->
-		<mt-search
-		  v-model="value"
-		  placeholder="搜索">
-		</mt-search>
+		<header data-am-widget="header" class="am-header am-header-default">
+		  	<div class="am-header-left am-header-nav" @click="goBack">
+	  			<i class=" fa am-icon-angle-left"></i>
+	      	</div>
+	      	<h1 class="am-header-title">
+			    <mt-search v-model="value" cancel-text="" placeholder=""></mt-search>
+			</h1>
+	      	<div class="am-header-right am-header-nav">
+			      	搜索
+			</div>
+		</header>
+		
 		<!-- 背景图片 -->
 		<div class="bg"></div>
   </div>
@@ -13,8 +21,17 @@
 <script>
 import Vue from 'vue'
 export default {
-  name: 'Search',
-  value:""
+	name: 'Search',
+	data(){
+		return {
+			value:""
+		}
+	},
+	methods:{
+			goBack(){
+				window.history.back();
+			}
+		}
 }
 </script>
 
@@ -27,9 +44,32 @@ export default {
 	z-index: 100;
 }
 .search .bg{
-	background: url('./../../static/img/jie1.png') no-repeat;
+	background: url('./../../static/img/jie4.png') no-repeat;
 	background-size: 100% 100%;
 	width: 100%;
-	height: 70vh;
+	height: 90vh;
+}
+.am-header.am-header-default{
+	background:#dcd9cf;
+	height: 44px;
+	line-height: 44px;
+}
+.am-header-left.am-header-nav,.am-header-default .am-header-title,.am-header-right.am-header-nav{
+	color: #874c0d;
+}
+.am-header-default .am-header-title{
+	font-size: 1.7rem;
+}
+.am-header-left.am-header-nav i{
+	font-weight: normal;
+}
+.am-header .am-header-title{
+	margin: 0 12% 0 4%;
+}
+.mint-searchbar-core{
+	color: #000;
+}
+.search .mint-search[data-v-b75182e0]{
+	margin-right: 0;
 }
 </style>
