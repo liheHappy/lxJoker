853a0f3e13a91e10c54fc704fa3cdfbda98d8860
joke <1095313335@qq.com>
2017-06-16 12:28:31 +0800

    页面修改

--- src/components/Recharge.vue
+++ src/components/Recharge.vue
@@ -1,17 +1,47 @@
 <template>
-  <div class="textshow">
-	<!-- 头部 -->
-	<hx-show-header></hx-show-header>
-  	<!-- 信息列表 -->
-	<hx-text-msg></hx-text-msg>
-	<!-- 热评列表 -->
-	<hx-commenthot-list></hx-commenthot-list>
-	
-	<!-- 鲜评列表 -->
-	<hx-commentnew-list></hx-commentnew-list>
-	
-	<!--评论标签-->
-	<hx-commentnew-input></hx-commentnew-input>
+  <div class="recharge">
+		<!-- 头部 -->
+		<hx-recharge-header></hx-recharge-header>
+		<!--余额-->
+		<div class="balance">
+			<h1>0</h1>
+			<p>
+				余额 <span class="am-icon-diamond"></span>
+			</p>
+		</div>
+		<div class="honor">
+			<ul>
+				<li>
+					<span class="am-icon-diamond"></span>
+					<span>累计钻石</span>
+					<i>0</i>
+					<i class="am-icon-diamond"></i>
+				</li>
+				<li>
+					<span class="am-icon-frown-o"></span>
+					<span>当前荣誉</span>
+					<i>暂无</i>
+				</li>
+				<li>
+					<span class="am-icon-envira"></span>
+					<span>下个荣誉</span>
+					<i>0/1万</i>
+					<i class="am-icon-diamond"></i>
+				</li>
+			</ul>
+		</div>
+		<!--充值金额-->
+		<div class="money">
+			<p>充值金额</p>
+			<ul>
+				<li>
+					<span class="am-icon-diamond"></span>
+					<span>10</span>
+					<i>赠送10钻石（首充特惠）</i>
+					<button type="button" class="am-btn am-btn-warning am-round">￥1</button>
+				</li>
+			</ul>
+		</div>
   </div>
 </template>
 
@@ -19,7 +49,7 @@
 import Vue from 'vue'
 import jsonp from 'jsonp'
 export default {
-  name: 'Textshow',
+  name: 'recharge',
   data(){
   	return {
   		id:""
@@ -29,391 +59,141 @@ export default {
 
   },
   components:{
-  	"hx-text-msg":{
-  		template:`<div>
-				<div class="txt txtTop">
-					<header class="txt-header">
-						<img :src="obj.group.user.avatar_url" class="am-comment-avatar" width="48" height="48">
-						<span>{{obj.group.user.name}}</span>
-						<span class="hasBorderOne">＋加关注</span>
-					</header>
-					<p class="am-article-lead">
-						{{obj.group.text}}
-						<span class="neihan">{{obj.group.category_name}}</span>
-					</p>
-					<div>
-						<i class="am-icon-thumbs-up">	{{obj.group.digg_count}}</i>
-						<i class="am-icon-thumbs-down">{{obj.group.bury_count}}</i>
-						<i class="am-icon-commenting-o">{{obj.group.comment_count}}</i>
-						<i class="am-icon-share">
-						{{obj.group.share_count}}</i>
-					</div>
-				</div>
-			</div>`,
-		data(){
-			return {
-				obj:""
-			}
-		},
-		created(){
-			var id=window.location.pathname.replace("/Textshow/","");
-			localStorage.textData=localStorage.textArr;
-		   	var arr=JSON.parse(localStorage.textArr);
-		    arr.map((item,index)=>{
-		     	if(item.group.group_id==id){
-		     		this.obj=item;
-		      	}
-		    })
-		 }	
-  	},
-  	"hx-show-header":{
-  		template:`<div>
-  					<header data-am-widget="header"
-			          class="am-header am-header-default">
+  	"hx-recharge-header":{
+  		template:`
+	  		<div>
+					<header data-am-widget="header" class="am-header am-header-default">
 			      <div class="am-header-left am-header-nav" @click="goBack">
 			  			<i class=" fa am-icon-angle-left"></i>
 			      </div>
-
-			      <h1 class="am-header-title">
-			          详情
-			      </h1>
-
+			      <h1 class="am-header-title">充值</h1>
 			      <div class="am-header-right am-header-nav">
-			      	举报
+			      	<i class="am-icon-pencil-square-o"></i>
 			      </div>
-			  </header>
-			  </div>`,
-		methods:{
-			goBack(){
-				window.history.back();
+				  </header>
+				</div>
+			`,
+			methods:{
+				goBack(){
+					window.history.back();
+				}
 			}
-		}
-  	},
-  	"hx-commentnew-input":{
-  		template:`
-				<div class="footP">
-					<p>
-						<span class="am-icon-pencil"></span>
-						<input type="text" placeholder="期待你的神评论" />
-					</p>
-				</div>
-  		`
-  	},
-  	"hx-commenthot-list":{
-  		template:`<div class="list">
-	  				<span class="commentHot" v-if="show">热门评论( {{num}} )</span>
-			  		<div v-for="items in arr">
-						<article class="am-comment">
-						  <a href="#link-to-user-home">
-						    <img :src="items.avatar_url" alt="" class="am-comment-avatar" width="48" height="48"/>
-						  </a>
-						  <div class="am-comment-main">
-						    <header class="am-comment-hd">
-						      <div class="am-comment-meta">
-						        <a href="#link-to-user" class="am-comment-author">{{items.user_name}}</a>
-						   		<time>{{ dataChange(items.create_time) }}</time>
-						      </div>
-						      <div class="zanZhuan">
-						      	<i class="am-icon-thumbs-up"> {{ items.digg_count}}</i>
-								<i class="am-icon-share"></i>
-						      </div>
-						    </header>
-
-						    <div class="am-comment-bd">
-						      {{items.text}}
-						    </div>
-						  </div>
-						</article>
-						
-					</div>
-				</div>`,
-  		data(){
-  			return {
-  				arr:"",
-  				num:"",
-  				show:true
-  			}
-  		},
-  		methods:{
-  			dataChange(x){
-  				var d=new Date(x*1000);
-  				return this.format(d);
-  			},
-  			format(date){
-				var y = date.getFullYear();  
-			    var m = date.getMonth() + 1;  
-			    m = m < 10 ? '0' + m : m;  
-			    var d = date.getDate();  
-			    d = d < 10 ? ('0' + d) : d;  
-			    return y + '-' + m + '-' + d; 
-  			}
-  		},
-  		created(){
-		  	//段子热门评论的数据
-		  	var id=window.location.pathname.replace("/Textshow/","");
-		  	var url="http://m.neihanshequ.com/api/get_essay_comments/?app_name=neihanshequ_web&group_id="+id+"&offset=0&csrfmiddlewaretoken=a66e8d138afdb05562b9c00dc6bca50b";
-		  	
-		  	jsonp(url,null,(err,res)=>{
-		  		if(err){
-		  			console.log("数据获取失败");
-		  		}else{
-		  			
-		  			var data=res.data.top_comments;
-		  			this.arr=data;
-		  			var len=this.arr.length
-		  			if(len!=0){
-		  				this.num=len;
-		  			}else{
-		  				this.show=false;
-		  			}
-		  		}
-		  	})
-
-		  	//本地获取
-		  	// Vue.axios.get("../static/json/comment.json").then((res)=>{
-		   //    return res.data.data.top_comments;
-		   //  }).then((data)=>{
-		   //    this.arr=data;
-		   //    this.num=this.arr.length;
-		   //  })
-  		}
-  	},
-  	"hx-commentnew-list":{
-  		template:`
-  			<div class="list">
-  				<span class="commentHot" v-if="show">新鲜评论( {{num}} )</span>
-		  		<div v-for="items in arr">
-					<article class="am-comment">
-					  <a href="#link-to-user-home">
-					    <img :src="items.avatar_url" alt="" class="am-comment-avatar" width="48" height="48"/>
-					  </a>
-					  <div class="am-comment-main">
-					    <header class="am-comment-hd">
-					      <div class="am-comment-meta">
-					        <a href="#link-to-user" class="am-comment-author">{{items.user_name}}</a>
-					   		<time>{{ dataChange(items.create_time) }}</time>
-					      </div>
-					      <div class="zanZhuan">
-					      	<i class="am-icon-thumbs-up">{{items.digg_count}}</i>
-							<i class="am-icon-share"></i>
-					      </div>
-					    </header>
-					    <div class="am-comment-bd">
-					      {{items.text}}
-					    </div>
-					  </div>
-					</article>
-				</div>
-			</div>`,
-  		data(){
-  			return {
-  				arr:"",
-  				num:"",
-  				show:true
-  			}
-  		},
-  		methods:{
-  			dataChange(x){
-  				var d=new Date(x*1000);
-  				return this.format(d);
-  			},
-  			format(date){
-				var y = date.getFullYear();  
-			    var m = date.getMonth() + 1;  
-			    m = m < 10 ? '0' + m : m;  
-			    var d = date.getDate();  
-			    d = d < 10 ? ('0' + d) : d;  
-			    return y + '-' + m + '-' + d; 
-  			}
-  		},
-  		created(){
-		  	//段子新鲜评论的数据
-		  	var id=window.location.pathname.replace("/Textshow/","");
-		  	var url="http://m.neihanshequ.com/api/get_essay_comments/?app_name=neihanshequ_web&group_id="+id+"&offset=0&csrfmiddlewaretoken=a66e8d138afdb05562b9c00dc6bca50b";
-		  	jsonp(url,null,(err,res)=>{
-		  		if(err){
-		  			console.log("数据获取失败");
-		  		}else{
-		  			var data=res.data.recent_comments;
-		  			this.arr=data;
-		  			var len=this.arr.length
-		  			if(len!=0){
-		  				this.num=len;
-		  			}else{
-		  				this.show=false;
-		  			}
-		  		}
-		  	})
-
-		  	//获取本地模拟json数据
-		  	// Vue.axios.get("../static/json/comment.json").then((res)=>{
-		   	//    return res.data.data.recent_comments;
-		   	//  }).then((data)=>{
-		   	//    this.arr=data;
-		   	//    this.num=this.arr.length;
-		   	//  })
-  		}
   	}
   }
 }
 </script>
 
 <style>
-.textshow{
+.recharge{
 	height: 100vh;
-	background: #eee;
+	background: #ffffff;
 	position: absolute;
 	z-index: 100;
 	overflow-x: hidden;
 	width: 100%;
 }
-.txt{
-	background: #fff;
-	overflow: auto;
-	margin-bottom: .5rem;
-}
-.txtTop{
-	padding-top: 49px;
-}
-
-.txt-header{
-	padding-top: .5rem;
-	height: 40px;
-	line-height: 40px;
-	margin-bottom: .5rem;
-}
-.txt-header img{
-	margin-top: 4px;
-	margin-left: 1rem;
-	margin-right: 0.5rem;
-}
-.textshow .hasBorderOne{
-	border: 1px solid #fe2d55;
-	color: #fe2d55;
-	float: right;
-	text-align: center;
-	line-height: 12px;
-	margin-right: 1rem;
-	padding: .5rem .5rem;
-	margin-top: .3rem;
-	border-radius: 5px;
-}
-.textshow .am-article-lead{
-	margin: 0;
-	color: #333333;
-	border: none;
-	background: none;
-	font-size: 1.6rem;
-}
-.textshow .neihan{
-	border: 1px solid #6b5547;
-	white-space: nowrap;
-	background: #f5f5f5;
-	border-radius: 4px;
-	color: #6b5547;
-	padding:.1rem .3rem;
-	margin: .2rem .3rem;
-	font-size: .8rem;
-}
-.am-icon-share{
-	float: right;
-}
-.txt div i{
-	padding: .8rem;
-	color: #ccc;
-}
-.fontColor{
-	color: #000;
-}
-.am-header.am-header-default{
-	background:#dcd9cf;
+.recharge .am-header.am-header-default{
+	background: #ffdc3c;
 	position: fixed;
 	z-index: 100;
 }
-.am-header-left.am-header-nav,.am-header-default .am-header-title,.am-header-right.am-header-nav{
-	color: #874c0d;
+.recharge .am-header-left.am-header-nav,.am-header-default .am-header-title,.am-header-right.am-header-nav{
+	color: #786b2d;
 }
-.am-header-default .am-header-title{
-	font-size: 1.7rem;
+.recharge .am-header-default .am-header-title{
+	font-size: 1.5rem;
+	color: #786b2d;
 }
-.am-header-left.am-header-nav i{
+.recharge .am-header-nav i{
 	font-weight: normal;
-}
-.textshow .list{
-	background: #fff;
-}
-.textshow>div:nth-of-type(4){
-	margin-bottom: 38px;
-}
-.am-comment{
-	padding: 0rem 0.5rem 1rem 0.5rem;
-}
-.textshow .list div:nth-of-type(1) .am-comment{
-	padding-top: 1rem;
-}
-.am-comment .am-comment-main{
-	position: static;
-	border: none;
-	border-bottom: 1px solid #dedede;
-}
-.am-comment .am-comment-main .am-comment-bd{
-	padding: 8px 0 8px 0;
-	line-height: 20px;
 	color: #333333;
 }
-.am-comment .am-comment-main .am-comment-hd{
-	background: #FFFFFF;
-	border: none;
-}
-.am-comment .am-comment-main .am-comment-hd .am-comment-meta{
-	padding: 0;
-	font-size: 12px;
-}
-.am-comment .am-comment-main .am-comment-hd .zanZhuan i{
-	padding: 0 5px 0 10px;
-}
-.am-comment .am-comment-main .am-comment-hd .zanZhuan .am-icon-thumbs-up:before{
-	margin-right: 6px;
-}
-.am-comment-meta .am-comment-author{
-	display: block;
-	font-size: 16px;
-	font-weight: normal;
-	color: #666666;
-}
-.list i{
-	padding: .5rem;
-	color: #ccc;
-}
-.commentHot{
-	font-size: .5rem;
-	margin-left: .5rem;
-}
-.textshow .footP{
-	position: fixed;
-	width: 100%;
-	background: #FFFFFF;
-	z-index: 1000;
-	border-top: 1px solid #e5e5e5;
-	padding: 0.5rem 1% 0.5rem 1%;
-	bottom: 0;
-}
-.textshow .footP p{
-	margin: 0;
-	border: 1px solid #cccccc;
-}
-.textshow .footP p span{
-	display: inline-block;
-	width: 10%;
-	height: 10%;
+/*余额*/
+.recharge .balance{
+	margin-top: 49px;
+	background: #ffdc3c;
 	text-align: center;
-	line-height: 2rem;
-	color: #cccccc;
+	padding-bottom: 3rem;
 }
-.textshow .footP p input{
-	width: 88%;
-	height: 3rem;
-	border: none;
+.recharge .balance h1{
+	font-size: 4rem;
+	font-weight: normal;
+	margin: 0;
+	color: #1e1e1e;
+}
+.recharge .balance p{
+	margin: 0;
+	color: #333333;
+}
+.recharge .balance p span{
+	color: #2e82ce;
+}
+/*荣誉*/
+.recharge .honor{
+	padding-top: 1.6rem;
+	padding-bottom: 1.6rem;
+	border-top: 1px solid #6B5547;
+}
+.recharge .honor ul{
+	padding: 0;
+	display: flex;
+	justify-content: space-around;
+}
+.recharge .honor ul li{
+	list-style: none;
+	text-align: center;
+	font-size: 1.4rem;
+	color: #686868;
+}
+.recharge .honor ul li i{
+	font-style: normal;
+	color: #D8D8D8;
+}
+.recharge .honor ul li span{
+	display: block;
+}
+.recharge .honor ul li span:nth-of-type(1){
+	width: 5.3rem;
+	height: 5.3rem;
+	border: 3px solid #f6f6f6;
+	border-radius: 100%;
+	color: #D8D8D8;
+	font-size: 3rem;
+	line-height: 4.6rem;
+}
+/*充值金额*/
+.recharge .money p{
+	margin: 0;
+	padding: 0.6rem;
+	font-size: 1.4rem;
+	background: #f6f6f6;
+	color: #999999;
+}
+.recharge .money ul{
+	padding: 0;
+	margin: 0;
+}
+.recharge .money ul li{
+	list-style: none;
+	font-size: 1.4rem;
+	border-top: 1px solid #f6f6f6;
+	padding: 0.6rem;
+	line-height: 2rem;
+}
+.recharge .money ul li i{
+	font-style: normal;
+	color: #ffd630;
+	margin-left: 4.6rem;
+	font-size: 1rem;
+}
+.recharge .money ul li span{
+	color: #1e1e1e;
+}
+.recharge .money ul li .am-icon-diamond{
+	color: #2e82ce;
+}
+.recharge .money ul li .am-btn{
+	padding: 0.2rem 2rem 0.2rem 2rem;
 }
 </style>
 
