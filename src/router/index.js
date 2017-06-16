77aaeacab74d355acb48f958764b880084563358
joke <1095313335@qq.com>
2017-06-16 11:02:08 +0800

    页面修改

--- src/router/index.js
+++ src/router/index.js
@@ -20,6 +20,7 @@ import Setpass from '@/components/Setpass'
 import Setnickname from '@/components/Setnickname'
 import Search from '@/components/Search'
 import Userinfo from '@/components/Userinfo'
+import Recharge from '@/components/Recharge'
 
 Vue.use(Router)
 
@@ -127,6 +128,11 @@ export default new Router({
       path: '/Userinfo',
       name: 'Userinfo',
       component: Userinfo
+    },
+    {
+      path: '/Recharge',
+      name: 'Recharge',
+      component: Recharge
     }
   ]
 })
